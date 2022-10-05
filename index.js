79fb6355c9ba02dffe6dbd96fa035a9d91d2c3d9
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-10-05 21:40:34 +0200

    appli quiz

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+class Question {
+  constructor(newQuestion, answerArray, goodAnswer) {
+    this.newQuestion = newQuestion;
+    this.answerArray = answerArray;
+    this.goodAnswer = goodAnswer;
+  }
+}
+
 const questions = [
   new Question(
     "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
@@ -20,3 +28,55 @@ const questions = [
     "Math.round()"
   ),
 ];
+
+let index = 0;
+let monScore = 0;
+
+function questionDisplay() {
+  question.innerHTML = questions[index].newQuestion;
+}
+
+function answerDisplay() {
+  guess0.innerHTML = questions[index].answerArray[0];
+  guess1.innerHTML = questions[index].answerArray[1];
+  guess2.innerHTML = questions[index].answerArray[2];
+  guess3.innerHTML = questions[index].answerArray[3];
+}
+
+function progressDisplay() {
+  progress.innerHTML = `
+    Question ${index + 1} sur ${questions.length}
+  `;
+}
+
+function scoreResult(choice) {
+  if (choice == questions[index].goodAnswer) {
+    monScore++;
+  }
+}
+
+function resultDisplay() {
+  document.querySelector("h1").innerHTML = "QUIZ TERMINÉ !";
+  question.innerHTML = "";
+  score.innerHTML = `Votre score est de : ${monScore} / ${questions.length}`;
+  document.querySelector(".choices").innerHTML = "";
+  progress.innerHTML = "";
+}
+
+document.querySelectorAll(".btn").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    scoreResult(e.target.textContent);
+    index++;
+    if (index < questions.length) {
+      questionDisplay();
+      answerDisplay();
+      progressDisplay();
+    } else {
+      resultDisplay();
+    }
+  });
+});
+
+questionDisplay();
+answerDisplay();
+progressDisplay();
